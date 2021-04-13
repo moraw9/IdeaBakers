be4aea745a2a1c082cce1359f979953a4eda8e3b
moraw9 <marzena.orawczak@wp.pl>
2021-04-13 21:48:56 +0200

    displaying data from the firebase does not work

--- app/routes/ideas/index.js
+++ app/routes/ideas/index.js
@@ -1,21 +1,5 @@
-
-// export default Ember.Route.extend({
-//     model: function() {
-//       return this.store.findAll('idea');
-//     }
-// });
-
-import Route from '@ember/routing/route';
-
 export default Ember.Route.extend({
     model: function() {
-      return{
-      title: 'Grand Old Mansion',
-      user: 'Veruca Salt',
-      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ducimus consectetur tenetur impedit accusamus aut commodi omnis, nulla similique vel.',
-      numberOfKudos: 14,
-      imageURL: 'https://ksr-ugc.imgix.net/assets/032/801/830/240a13bc4ad329b1e30e806f6d0d0d19_original.jpg?ixlib=rb-2.1.0&w=680&fit=max&v=1616163012&auto=format&frame=1&q=92&s=51e61d92aa13c2e4b9502d7e06baf37f',
-    };
-      
-  }
+      return this.store.findAll('idea');
+    }
 });
