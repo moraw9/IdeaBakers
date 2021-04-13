ff9dc2122bad23ebe77640570632200cbb1584cf
Adam Woźny <wozny1989@gmail.com>
2021-04-14 01:10:21 +0200

    remove reduntant code, linting

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,7 @@
-
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
- async model() {
-   
-    const result =await this.store.query('idea', {});
-    debugger;
-      return result;
-      // return [];
-    }
-};
+  model() {
+    return this.store.findAll('idea');
+  }
+}
