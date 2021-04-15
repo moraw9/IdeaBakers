704aa661dc98e058d3c6b00b71b4a41a2de131c6
moraw9 <marzena.orawczak@wp.pl>
2021-04-15 21:29:34 +0200

    added content to dedicated idea page

--- app/components/details/component.js
+++ app/components/details/component.js
@@ -7,27 +7,9 @@ export default class IndexComponent extends Component {
 
   @service store ;
 
-  // constructor(){
-  //   super(...arguments);
-  //   this.model = this.store.findAll('idea');
-  // }
-  // @tracked query = '';
-
-  // @action 
-  // setSearchQuery(event) {
-
-  //   this.query = event.target.value;
-  // };
-
-  // get results(){
-  //   const {model, query} = this;
-
-  //   if(query){
-  //     return model.filter((idea) => idea.title.includes(query));
-      
-  //   }
-    
-  //   return model;
-  // }
-
+  constructor(){
+    super(...arguments);
+    // debugger;
+    this.model = this.store.findRecord('idea', this.args.model.id);
+  }
 }
