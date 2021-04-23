23faf794cc3012920664c9de58f6317e7ebb3956
moraw9 <marzena.orawczak@wp.pl>
2021-04-23 15:49:15 +0200

    cleared code

--- app/components/index.js
+++ app/components/index.js
@@ -2,32 +2,22 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-
 export default class IndexComponent extends Component {
+  @service store;
 
-  @service store ;
-
-  constructor(){
+  constructor() {
     super(...arguments);
     this.model = this.store.findAll('idea');
   }
   @tracked query = '';
-
-  @action 
-  setSearchQuery(event) {
-
+  @action setSearchQuery(event) {
     this.query = event.target.value;
-  };
-
-  get results(){
-    const {model, query} = this;
-
-    if(query){
+  }
+  get results() {
+    const { model, query } = this;
+    if (query) {
       return model.filter((idea) => idea.title.includes(query));
-      
     }
-    
     return model;
   }
-
 }
