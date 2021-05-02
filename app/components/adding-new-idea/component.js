1451c08f0bfb53d99142a5626d8458f9afa7a719
moraw9 <marzena.orawczak@wp.pl>
2021-05-02 18:16:32 +0200

    close modal after saving and clear if close without saving

--- app/components/adding-new-idea/component.js
+++ app/components/adding-new-idea/component.js
@@ -31,19 +31,24 @@ export default class AddingNewIdeaComponent extends Component {
       if (this.changeset.get('isValid')) {
         this.changeset.imageURL =
           'data:image/png;base64,' + this.changeset.imageURL;
-        this.changeset.save();
-        alert('Congratulations! The idea has been added successfully!');
-        this.changeset.rollback();
-        this.clear();
+        this.changeset.save().then(() => {
+          document.getElementById('closeModalButton').click();
+          alert('Congratulations! The idea has been added successfully!');
+        });
       }
     });
   }
+
   @action
-  rollback() {
-    return this.changeset.rollback();
-  }
   clear() {
     document.querySelectorAll('input').forEach((input) => (input.value = ''));
     document.querySelector('textarea').value = '';
+  }
+
+  @action
+  rollback() {
+    document.querySelectorAll('input').forEach((input) => (input.value = ''));
+    document.querySelector('textarea').value = '';
+    this.changeset.rollback();
   }
 }
