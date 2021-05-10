3214962021f2fd500fdc0b59e91360eba580c39b
moraw9 <marzena.orawczak@wp.pl>
2021-05-10 20:53:43 +0200

    changed type of input to upload image

--- app/components/adding-new-idea/component.js
+++ app/components/adding-new-idea/component.js
@@ -4,9 +4,11 @@ import { action } from '@ember/object';
 import { Changeset } from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 import IdeaValidators from '../../validations/idea';
+import { task } from 'ember-concurrency';
 export default class AddingNewIdeaComponent extends Component {
   @service session;
   @service store;
+  @service firebase;
 
   constructor() {
     super(...arguments);
@@ -22,24 +24,45 @@ export default class AddingNewIdeaComponent extends Component {
   }
 
   @action
-  setValue({ target: { name, value } }) {
+  setValue({ target: { name, value, files } }) {
     this.changeset[name] = value;
+    if (name === 'imageURL') {
+      this.changeset[name] = files[0];
+    }
+  }
+  @task({ restartable: true }) *setImageURLTask() {
+    if (typeof this.changeset.imageURL === 'undefined') return;
+
+    const storageRef = this.firebase
+      .storage()
+      .ref(
+        'project/' +
+          this.changeset.imageURL.name +
+          '/' +
+          this.changeset.imageURL.lastModified
+      );
+
+    yield storageRef
+      .put(this.changeset.imageURL)
+      .catch((error) => console.log(error));
+
+    const url = yield storageRef.getDownloadURL();
+    return url;
   }
 
   @action
-  addIdea() {
+  async addIdea() {
+    await this.setImageURLTask.perform();
     this.changeset.user = this.args.currentUser.displayName;
+    this.changeset.imageURL = this.setImageURLTask.lastSuccessful.value;
+
     this.changeset.validate().then(() => {
       if (this.changeset.get('isValid')) {
-        this.changeset.imageURL =
-          'data:image/png;base64,' + this.changeset.imageURL;
-
         this.changeset.save().then(() => {
           document.getElementById('closeModalButton').click();
           alert('Congratulations! The idea has been added successfully!');
           this.createChangeset();
           this.rollback();
-          debugger;
         });
       }
     });
