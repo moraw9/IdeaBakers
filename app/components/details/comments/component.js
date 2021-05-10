b27520d319297984a1ffa87654869ab6ff37b79b
moraw9 <marzena.orawczak@wp.pl>
2021-05-10 22:39:13 +0200

    fixed displaying comments

--- app/components/details/comments/component.js
+++ app/components/details/comments/component.js
@@ -3,19 +3,21 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { task } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
-import firebase from 'firebase';
 // eslint-disable-next-line ember/no-computed-properties-in-native-classes
 
 export default class CommentsComponent extends Component {
   @service store;
   @service session;
+  @service firebase;
+
   @tracked hasComments = false;
   @tracked comments;
+  @tracked currentUser;
 
   constructor() {
     super(...arguments);
-    this.findCommentsTask.perform();
     this.currentUser = firebase.auth().currentUser;
+    this.findCommentsTask.perform();
   }
   @task({ restartable: true }) *findCommentsTask() {
     const comments = yield this.store.findAll('comment');
