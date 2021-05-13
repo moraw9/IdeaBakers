a7ce987a175e632cbbd4c3b220ab60719768d5ae
moraw9 <marzena.orawczak@wp.pl>
2021-05-13 16:42:19 +0200

    deleted comment

--- app/helpers/getExcerpt.js
+++ app/helpers/getExcerpt.js
@@ -1,7 +1,6 @@
 import { helper } from '@ember/component/helper';
 
 function getExcerpt([string]) {
-  // console.log('string', string);
   if (!string) return '';
   return string
     .slice(0, string.length >= 300 ? 300 : string.length)
