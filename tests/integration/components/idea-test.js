aba65c60a24ec266c6af8325178cb2bb63f533cd
moraw9 <marzena.orawczak@wp.pl>
2021-05-25 20:05:54 +0200

    passed idea-test

--- tests/integration/components/idea-test.js
+++ tests/integration/components/idea-test.js
@@ -9,22 +9,38 @@ module('Integration | Component | idea', function (hooks) {
   setupMirage(hooks);
 
   test('it renders information about a idea property', async function (assert) {
-    this.idea = this.server.create('idea', {
+    let serverModel = this.server.create('idea', {
       title: 'Ember Simple Title',
-      description: 'Lorem ipsum dolor',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
       numberOfKudos: 15,
       imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      userRecordId: 'aOeEiqbwXig2PCgCBEtz',
+      userRecordId: '1',
       userUid: 'VmfhNjdWTedYBF4r3Ny0WHNBQxH2',
     });
 
-    await render(hbs`<Idea @idea={{this.idea}} />`);
+    this.server.create('user', {
+      name: 'Asia',
+      surname: 'Jamroz',
+      email: 'asai@wp.pl',
+      photoUrl: null,
+      pswd: '1234578',
+      rpswd: '12345678',
+      userKudos: 35,
+    });
+
+    let store = this.owner.lookup('service:store');
+    let idea = await store.findRecord('idea', serverModel.id);
+    this.set('idea', idea);
+
+    await render(hbs`<Idea @idea={{idea}} />`);
 
     assert.dom('.card').exists();
     assert.dom('[data-test-title]').hasText('Ember Simple Title');
-    assert.dom('[data-test-description]').hasText('Lorem ipsum dolor...');
-    // assert.dom('[data-test-author]').hasText('Admin');
+    assert
+      .dom('[data-test-description]')
+      .hasText('Lorem ipsum dolor sit amet, consectetur adipiscing elit...');
+    assert.dom('[data-test-author]').hasText('Asia');
     assert.dom('[data-test-img]').hasAttribute('style');
 
     let style = document.querySelector('[data-test-img]').getAttribute('style');
